a11aebe7cf7aaf149e95d9dacb5df7d2ba687902
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2020-12-21 15:29:11 +0530

    KRBS

--- script/script.js
+++ script/script.js
@@ -1,12 +1,22 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true
-});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        480: {
+            items: 2
+        },
+        769: {
+            items: 3
+        }
+    }
+})
 
 var btxt = document.getElementById('btxt');
 var word = btxt.getElementsByTagName('span');
